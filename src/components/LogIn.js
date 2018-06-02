b4b86f7a79c4b76f9cae6c1b73faf8d776202b39
Andrew Pryshchepa <vinglfm@gmail.com>
2018-06-02 15:38:45 +0300

    Added basic validation to log in component

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,12 +1,27 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import Validator from 'fastest-validator';
 import styles from './SignInModal.css';
+
+const validator = new Validator();
 
 export default class LogIn extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            email:'',
+            password:''
+        };
+
+        this.schema = {
+            email: { type: 'email' },
+            password: { type: "string", min: 8, max: 16 }
+        };
+
         this.logIn = this.logIn.bind(this);
+        this.canSubmit = this.canSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     logIn() {
@@ -14,7 +29,18 @@ export default class LogIn extends Component {
         console.log(this.refs.password.value);
     }
 
+    handleChange(elem) {
+        let changed = {};
+        changed[elem.target.name] = elem.target.value;
+        this.setState(changed);
+    }
+
+    canSubmit() {
+        return validator.validate(this.state, this.schema) === true;
+    }
+
     render() {
+        const isEnabled = this.canSubmit();
         return (
             <div>
                 <div className={styles.modal__header}>
@@ -24,11 +50,11 @@ export default class LogIn extends Component {
                 <hr className={styles.modal__divider}/>
                 <form className={styles.modal__form} name='LogInForm' onSubmit={this.logIn} role='form'>
                     <label className={styles.modal__form__label}>Email<span className={styles.modal__form__label__required}>*</span></label>
-                    <input className={styles.modal__form__input} type='email' ref='email'/>
+                    <input className={styles.modal__form__input} name='email' type='email' ref='email' onChange={this.handleChange}/>
                     <label className={styles.modal__form__label}>Password<span className={styles.modal__form__label__required}>*</span></label>
-                    <input className={styles.modal__form__input} type='password' ref='password' placeholder='Password' minLength='8' maxLength='16'/>
+                    <input className={styles.modal__form__input} name='password' type='password' ref='password' placeholder='Password' onChange={this.handleChange} minLength='8' maxLength='16'/>
                     <div className={styles.modal__form__group}>
-                    <input className={styles.modal__form__group__submit} type='submit' value='Create Account'/>
+                    <input className={styles.modal__form__group__submit} type='submit' disabled={!isEnabled} value='Create Account'/>
                     </div>
                 </form>
             </div>
