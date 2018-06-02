de9cd1d387cbc8b50a74502ec19386a03e538887
Andrew Pryshchepa <vinglfm@gmail.com>
2018-06-02 13:05:27 +0300

    Add sign in modal test

--- src/components/SignInModal.test.js
+++ src/components/SignInModal.test.js
@@ -1,11 +1,25 @@
-import enzyme, {shallow} from 'enzyme';
+import enzyme, {mount, shallow} from 'enzyme';
 import ReactSixteenAdapter from 'enzyme-adapter-react-16';
 enzyme.configure({ adapter: new ReactSixteenAdapter() });
 
 import React from 'react';
 import SignInModal from './SignInModal';
 
-it('renders SignInModal', () => {
+it('renders SignInModal in base state', () => {
   const snapshot = shallow(<SignInModal open handleClose={()=>{}} />);
   expect(snapshot).toMatchSnapshot();
 });
+
+it('renders SignInModal in logIn state', () => {
+    const snapshot = shallow(<SignInModal open handleClose={()=>{}} />);
+    snapshot.instance().logInComponent();
+    snapshot.update();
+    expect(snapshot).toMatchSnapshot();
+  });
+
+  it('renders SignInModal in signUp state', () => {
+    const snapshot = shallow(<SignInModal open handleClose={()=>{}} />);
+    snapshot.instance().signUpComponent();
+    snapshot.update();
+    expect(snapshot).toMatchSnapshot();
+  });
