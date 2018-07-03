9536383c56d39e78901bd458e5c8f3d48d738d80
Andrew Pryshchepa <vinglfm@gmail.com>
2018-07-03 21:37:00 +0300

    Create redux store

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import './index.css';
 import '@fortawesome/fontawesome';
 import '@fortawesome/fontawesome-free-solid';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-        <App />,
+        <Provider store={store}>
+        <App />
+        </Provider>,
      document.getElementById('root'));
 registerServiceWorker();
