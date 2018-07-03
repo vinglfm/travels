085d089717b78c2a67bc8ea78c9bea7452ca85b3
Andrew Pryshchepa <vinglfm@gmail.com>
2018-07-03 21:36:35 +0300

    Created user reducers, added redux dependency

---
+++ src/reducers/user.js
@@ -0,0 +1,12 @@
+const user = (state = {}, action) => {
+    switch(action.type) {
+        case 'LOG_IN':
+            return action.user;
+        case 'LOG_OUT':
+            return {};
+        default:
+            return state;
+    }
+}
+
+export default user;
