36f9dc863fb588ed0b33f01a657901591aea641a
Andrew Pryshchepa <vinglfm@gmail.com>
2018-08-24 11:58:04 +0300

    TextInput changed to pure component

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import styles from './Auth/AuthModal.css';
 
-export default class TextInput extends Component {
+export default class TextInput extends PureComponent {
   render() {
     return (
       <div>
