924e025157b3a29a35bba21aeb99e5f330f9c43e
Andrew Pryshchepa <vinglfm@gmail.com>
2018-09-09 13:59:26 +0300

    Test request body of user service

--- src/_services/userService.test.js
+++ src/_services/userService.test.js
@@ -14,24 +14,30 @@ describe('UserService', () => {
       const fetchResponse = {text: () => Promise.resolve(JSON.stringify(json)), ok: true};
       fetch = jest.fn(() => new Promise((resolve) => resolve(fetchResponse)));
       fetch.pathParam = () => fetch.mock.calls[0][0];
-      fetch.body = () => fetch.mock.calls[0][1];
+      fetch.requestBody = () => fetch.mock.calls[0][1];
     });
 
     it('signIn', async () => {
       const response = await userService.signIn(email, password);
       expect(fetch.pathParam()).toEqual('http://localhost:4000/users/authenticate/signIn');
-      // expect(fetch).toBeCalledWith('http://localhost:4000/users/authenticate/signIn', {'body': '{\"email\":\"email\",\"password\":\"password\"}', 'headers': {'Content-Type': 'application/json'}, 'method': 'POST'});
-      const requestBody = fetch.body();
-      expect(requestBody.body).toEqual(email);
-      expect(requestBody.body).toEqual(password);
+      const requestBody = fetch.requestBody();
+      expect(requestBody.method).toEqual('POST');
+      const body = JSON.parse(requestBody.body);
+      expect(body.email).toEqual(email);
+      expect(body.password).toEqual(password);
       expect(response).toEqual(json);
     });
 
     it('signUp', async () => {
       const response = await userService.signUp(email, password, firstName, lastName);
       expect(fetch.pathParam()).toEqual('http://localhost:4000/users/authenticate/signUp');
-      // expect(fetch).toBeCalledWith('http://localhost:4000/users/authenticate/signUp', 
-      //   {'body': '{\"email\":\"email\",\"password\":\"password\",\"firstName\":\"firstName\",\"lastName\":\"lastName\"}', 'headers': {'Content-Type': 'application/json'}, 'method': 'POST'});
+      const requestBody = fetch.requestBody();
+      expect(requestBody.method).toEqual('POST');
+      const body = JSON.parse(requestBody.body);
+      expect(body.email).toEqual(email);
+      expect(body.password).toEqual(password);
+      expect(body.firstName).toEqual(firstName);
+      expect(body.lastName).toEqual(lastName);
       expect(response).toEqual(json);
     });
   });
@@ -42,13 +48,16 @@ describe('UserService', () => {
     beforeEach(() => {
       const response = {headers: {get() {return 'token';}}, body: {}, json() {return Promise.resolve(user);}};
       fetch = jest.fn(() => new Promise((resolve) => resolve(response)));
-      fetch.pathParam = () => fetch.mock.calls[0][0];
+      fetch.pathParam = () => fetch.mock.calls[0][0];    
+      fetch.requestBody = () => fetch.mock.calls[0][1];
     });
 
     it('SignIn', async () => {  
       const response = await userService.fbSignIn(token);
       expect(fetch.pathParam()).toEqual('http://localhost:4000/users/authenticate/facebook');
-      // '{"body": {Symbol(impl): {"_buffer": {"data": [123, 10, 32, 32, 34, 97, 99, 99, 101, 115, 115, 95, 116, 111, 107, 101, 110, 34, 58, 32, 34, 49, 34, 10, 125], "type": "Buffer"}, "isClosed": false, "type":"application/json", Symbol(wrapper): [Circular]}}, "cache": "default", "method": "POST", "mode": "cors"}');
+      //TODO: verify that body has _buffer with data
+      const requestBody = fetch.requestBody();
+      expect(requestBody.method).toEqual('POST');
       expect(response).toEqual(user);
     });  
   });
